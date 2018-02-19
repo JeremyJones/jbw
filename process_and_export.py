ff9dd7bd9d2fff096c6d8bdf3d13d53a90d4df44
Jeremy Jones <jer@jerjones.me>
2018-02-19 16:07:39 +0000

    pauseval

--- process_and_export.py
+++ process_and_export.py
@@ -9,23 +9,24 @@ from json import dumps as jsondumps
 
 def main() -> None:
     keyvals = Ingestor(KV_DIR)
-    mongo = MongoCollection(EXPORT_DIRECTORY)
+    collection = MongoCollection(EXPORT_DIRECTORY)
 
     for symbol, feed in keyvals.items():
         if 'quandl_error' in feed.data:
             continue
-        elif mongo.has(feed.symbol):
+        elif collection.has(feed.symbol):
             continue
 
         feed.add_variance()
         feed.add_natural_log()
-        mongo.add(feed.symbol, feed.data)
+        collection.add(feed.symbol, feed.data)
         print("Added {} to mongo.".format(symbol), flush=True)
 
     # tell the ingestor it can housekeep the ones we've put into mongo
-    keyvals.done(mongo.added)
-    print("All done, added {} records.".
-          format(len(mongo.added)))
+    keyvals.done(collection.added)
+    print("All done, added {} record{}.".
+          format(len(collection.added),
+                 '' if len(collection.added) == 1 else 's'))
 
 
 if __name__ == '__main__':
